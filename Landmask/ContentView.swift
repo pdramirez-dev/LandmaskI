c14d4c6cda1c08ced593db592ab937c10b4da7f2
Pablo E Diaz <pediaz@datamart.cl>
2023-08-18 22:36:12 -0400

    Initial Commit

---
+++ Landmask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmask
+//
+//  Created by Pablo E Diaz Ramairez on 8/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmask/LandmaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmaskApp.swift
+//  Landmask
+//
+//  Created by Pablo E Diaz Ramairez on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
